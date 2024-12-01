9ab45b8a3a11b5a0bd8e4bacb0545522cbdc6b01
Chris Sykes <c.j.sykes@gmail.com>
2024-12-01 20:30:50 +0000

    Day 1: use Ada.Containers.Vectors instead of arrays
    Vectors.Extended_Index was really helpful!

--- Day_01/main.adb
+++ Day_01/main.adb
@@ -1,33 +1,32 @@
 pragma Ada_2022;
 
-with Ada.Containers.Generic_Constrained_Array_Sort;
-with Ada.Exceptions;
-with Ada.IO_Exceptions;
+with Ada.Containers.Vectors;
 with Ada.Text_IO;
 
 procedure Main is
-    Input_Size : constant Positive := 1000;
-
     type Location_ID is new Positive;
-    type Location_Index is range 1 .. Input_Size;
-    type Location_Array is array (Location_Index) of Location_ID;
 
-    procedure Read_Input (A, B : out Location_Array) is
-        package Input is new Ada.Text_IO.Integer_IO (Num => Location_ID);
+    package ID_Vectors is new
+        Ada.Containers.Vectors (Index_Type   => Natural,
+                                Element_Type => Location_ID);
+
+    package ID_Vectors_Sorting is new ID_Vectors.Generic_Sorting;
+
+    procedure Read_Input (A, B : out ID_Vectors.Vector) is
+        use Ada.Text_IO;
+        package Input is new Integer_IO (Num => Location_ID);
+
+        Value: Location_ID;
     begin
-        for I in A'Range loop
-            Input.Get (A (I));
-            Input.Get (B (I));
+        while not End_Of_File loop
+            Input.Get (Value);
+            A.Append(Value);
+            Input.Get (Value);
+            B.Append(Value);
         end loop;
-    exception
-        when Ada.IO_Exceptions.End_Error =>
-            null;
     end Read_Input;
 
-    procedure Sort is
-        new Ada.Containers.Generic_Constrained_Array_Sort (
-            Location_Index, Location_ID, Location_Array);
-
+    -- Distance between Location_IDs
     type Distance is new Natural;
 
     function Distance_Between(A, B : in Location_ID) return Distance is
@@ -35,39 +34,40 @@ procedure Main is
         return Distance(abs(B - A));
     end Distance_Between;
 
-    function Distance_Between(A, B : in Location_Array) return Distance is
+    function Distance_Between(A, B : in ID_Vectors.Vector) return Distance is
     begin
         return Result : Distance := 0 do
-            for I in A'Range loop
+            for I in A.First_Index .. A.Last_Index loop
                 Result := @ + Distance_Between (A(I), B(I));
             end loop;
         end return;
     end Distance_Between;
 
+    -- Part 2 - calculate a "similarity score" between two Location ID vectors
     function Similarity_Between_Sorted_Lists(
-        Left, Right : in Location_Array
+        Left, Right : in ID_Vectors.Vector
     ) return Natural is
         Result      : Natural := 0;
         Left_Count  : Natural;
         Right_Count : Natural;
         Left_Item   : Location_ID;
-        Left_Idx    : Positive := Positive(Left'First);
-        Right_Idx   : Positive := Positive(Right'First);
+        Left_Idx    : ID_Vectors.Extended_Index := Left.First_Index;
+        Right_Idx   : ID_Vectors.Extended_Index := Right.First_Index;
     begin
-        while Left_Idx <= Positive(Left'Last) loop
-            Left_Item  := Left (Location_Index(Left_Idx));
+        while Left_Idx <= Left.Last_Index loop
+            Left_Item  := Left (Left_Idx);
             Left_Count := 1;
 
             -- Count consecutive elements in Left
-            while Left_Idx < Positive(Left'Last) and then Left (Location_Index(Left_Idx + 1)) = Left_Item loop
+            while Left_Idx < Left.Last_Index and then Left (Left_Idx + 1) = Left_Item loop
                 Left_Idx   := @ + 1;
                 Left_Count := @ + 1;
             end loop;
 
             -- Count occurrences of Left_Item in Right
             Right_Count := 0;
-            while Right_Idx < Positive(Right'Last) and then Right (Location_Index(Right_Idx)) <= Left_Item loop
-                if Right (Location_Index(Right_Idx)) = Left_Item then
+            while Right_Idx < Right.Last_Index and then Right (Right_Idx) <= Left_Item loop
+                if Right (Right_Idx) = Left_Item then
                     Right_Count := @ + 1;
                 end if;
                 Right_Idx := @ + 1;
@@ -80,22 +80,19 @@ procedure Main is
         return result;
     end Similarity_Between_Sorted_Lists;
 
-    List_A         : Location_Array;
-    List_B         : Location_Array;
+    List_A, List_B : ID_Vectors.Vector;
     Total_Distance : Distance;
     Similarity     : Natural;
-
-    use Ada.Text_IO;
 
 begin
     -- Part 1 - total distance
     Read_Input (List_A, List_B);
-    Sort (List_A);
-    Sort (List_B);
+    ID_Vectors_Sorting.Sort (List_A);
+    ID_Vectors_Sorting.Sort (List_B);
     Total_Distance := Distance_Between (List_A, List_B);
-    Put_Line("Distance   : " & Total_Distance'Image);
+    Ada.Text_IO.Put_Line("Distance   : " & Total_Distance'Image);
 
     -- Part 2
     Similarity := Similarity_Between_Sorted_Lists (List_A, List_B);
-    Put_Line("Similarity : " & Similarity'Image);
+    Ada.Text_IO.Put_Line("Similarity : " & Similarity'Image);
 end Main;
