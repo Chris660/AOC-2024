b85ff412565caa7eda63616263062a28c386606c
Chris Sykes <c.j.sykes@gmail.com>
2024-12-07 18:51:00 +0000

    day 6: part 2 WIP

--- Day_06/part2.adb
+++ Day_06/part2.adb
@@ -1,6 +1,9 @@
 pragma Ada_2022;
 
+with Ada.Containers;
+with Ada.Containers.Ordered_Sets;
 with Ada.Text_IO;
+with Ada.Unchecked_Conversion;
 
 procedure Part2 is
     use Ada.Text_IO;
@@ -38,85 +41,132 @@ procedure Part2 is
     Turn : constant array (Direction) of Direction := (
         Up => Right, Right => Down, Down => Left, Left => Up);
 
-    procedure Find_Guard (M : Puzzle; Guard_X, Guard_Y : out Positive) is
+    -- A record to store the guard's position
+    type Map_Coord is record
+        X : Natural;
+        Y : Natural;
+    end record;
+
+    function "<" (Left, Right : in Map_Coord) return Boolean is
     begin
-        for Y in M'Range(2) loop
-            for X in M'Range(1) loop
-                if M (Y, X) = '^' then
-                    Guard_X := X;
-                    Guard_Y := Y;
+        if Left.X = Right.X then
+            return Left.Y < Right.Y;
+        else
+            return Left.X < Right.X;
+        end if;
+    end;
+
+    -- A set of steps, used to determine where the guard has patrolled.
+    package Location_Sets is new Ada.Containers.Ordered_Sets
+        (Element_Type => Map_Coord);
+    use Location_Sets;
+
+    -- A record to store the guards position and direction.
+    type Position is record
+        Pos : Map_Coord;
+        Dir : Direction;
+    end record;
+
+    function "<" (Left, Right : in Position) return Boolean is
+    begin
+        if Left.Pos = Right.Pos then
+            return Left.Dir < Right.Dir;
+        else
+            return Left.Pos < Right.Pos;
+        end if;
+    end;
+
+    -- A set of steps, used to determine where the guard has patrolled.
+    package Position_Sets is new Ada.Containers.Ordered_Sets
+        (Element_Type => Position);
+    use Position_Sets;
+
+    procedure Find_Guard (Map   : in Puzzle;
+                          Guard : out Position) is
+    begin
+        for Y in Map'Range(2) loop
+            for X in Map'Range(1) loop
+                if Map (Y, X) = '^' then
+                    Guard.Pos.X := X;
+                    Guard.Pos.Y := Y;
+                    Guard.Dir := Up;
                 end if;
             end loop;
         end loop;
         pragma Assert (false);
     end Find_Guard;
 
-    Guard_Facing     : Direction := Up;
-    Start_X, Start_Y : Natural;
-    Guard_X, Guard_Y : Natural; -- needs to be able to step out of the map
-    Map              : Puzzle := Read_Input;
 
-    -- Part 2
-    Move_Count        : Natural := 0;
-    Obstruct_After    : Natural := 0;
-    Obstruction_X     : Natural := 0;
-    Obstruction_Y     : Natural := 0;
-    Obstruction_Count : Natural := 0;
-    Loop_Found_Count  : Natural := 0;
+    procedure Patrol (Map   : in Puzzle;
+                      Start : in Position;
+                      Path  : out Position_Sets.Set) is
+        Guard : Position := Start;
+    begin
+        while Guard.Pos.X in Map'Range(2) and then Guard.Pos.Y in Map'Range(1) loop
+            -- Record the guard's position and direction
+            if Map (Guard.Pos.Y, Guard.Pos.X) = '.' then
+                begin
+                    Path.Insert (Guard);
+                end;
+            end if;
+
+            declare
+                Next_X : Natural := Guard.Pos.X + Offset_X (Guard.Dir);
+                Next_Y : Natural := Guard.Pos.Y + Offset_Y (Guard.Dir);
+            begin
+                -- Turn to avoid obstacles, checking bounds.
+                while Next_X in Map'Range(2) and then
+                      Next_Y in Map'Range(1) and then 
+                      Map (Next_Y, Next_X) = '#' loop
+                    Guard.Dir := Turn(Guard.Dir);
+                    Next_X := Guard.Pos.X + Offset_X (Guard.Dir);
+                    Next_Y := Guard.Pos.Y + Offset_Y (Guard.Dir);
+                end loop;
+
+                -- Step forward
+                Guard.Pos.Y := Next_Y;
+                Guard.Pos.X := Next_X;
+            end;
+        end loop;
+    end Patrol;
+
+    Guard_Start      : Position;
+    Map              : Puzzle := Read_Input;
+    Trail            : Position_Sets.Set;
+    Visited          : Location_Sets.Set;
+    Loop_Found_Count : Natural := 0;
 
 begin
     -- Locate the starting position
-    Find_Guard (Map, Start_X, Start_Y);
-    loop
-        -- Reset start position
-        Guard_X       := Start_X;
-        Guard_Y       := Start_Y;
-        Guard_Facing  := Up;
-        Obstruction_X := 0;
-        Obstruction_Y := 0;
+    Find_Guard (Map, Guard_Start);
 
-        while Guard_X in Map'Range(2) and then Guard_Y in Map'Range(1) loop
-            declare
-                Next_X : Natural := Guard_X + Offset_X (Guard_Facing);
-                Next_Y : Natural := Guard_Y + Offset_Y (Guard_Facing);
-            begin
-                if Obstruct_After = Move_Count and then
-                        Next_X in Map'Range(2) and then Next_Y in Map'Range(1) and then
-                        Map (Next_Y, Next_X) = '.' then
-                    Obstruction_X := Next_X;
-                    Obstruction_Y := Next_Y;
-                    Guard_Facing := Turn(Guard_Facing);
-                    Next_X := Guard_X + Offset_X (Guard_Facing);
-                    Next_Y := Guard_Y + Offset_Y (Guard_Facing);
+    -- Replace the guard's symbol in the map with empty floor.
+    Map (Guard_Start.Pos.Y, Guard_Start.Pos.X) := '.';
 
-                elsif Next_X = Obstruction_X and then Next_Y = Obstruction_Y then
-                    -- we've looped.
-                    Loop_Found_Count := @ + 1;
-                    Put_Line ("Loop found");
-                    exit;
-                end if;
+    -- Part 1, walk the map with no additional obstructions.
+    -- Determine the set of visited locations (ignoring direction).
+    Patrol (Map, Guard_Start, Trail);
+    for Step of Trail loop
+        Visited.include (Step.Pos);
+    end loop;
+    Put ("Part 1, visited count:");
+    Put_Line (Length(Visited)'Image);
 
-                Move_Count := @ + 1;
-
-                if Next_X in Map'Range(2) and then Next_Y in Map'Range(1) then
-                    if Map (Next_Y, Next_X) = '#' then
-                        Guard_Facing := Turn(Guard_Facing);
-                    else
-                        -- Step forward
-                        Guard_Y := Next_Y;
-                        Guard_X := Next_X;
-                    end if;
-                else
-                    exit;
-                end if;
-                Put_Line (Move_Count'Image & Guard_X'Image & Guard_Y'Image & Guard_Facing'Image & Loop_Found_Count'Image);
-            end;
-        end loop;
-
-        Obstruct_After := @ + 1; -- Move one more step before adding the obstacle next time
-        exit when Move_Count <= Obstruct_After;
+    -- Part 2, try obstructing each tile visited in part 1.
+    for Pos of Visited loop
+        Map (Pos.Y, Pos.X) := '#';
+        Trail.Clear;
+        begin
+            Patrol (Map, Guard_Start, Trail);
+        exception
+            when Constraint_Error =>
+                -- FIXME: dedicated exception.
+                -- Loop detected.
+                Loop_Found_Count := @ + 1;
+        end;
+        Map (Pos.Y, Pos.X) := '.';
     end loop;
 
-    -- Put_Line ("Map: " & Map'Image);
-    Put_Line ("Part 2 loops: " & Loop_Found_Count'Image);
+    Put ("Part 2, loop count:");
+    Put_Line (Loop_Found_Count'Image);
 end Part2;
